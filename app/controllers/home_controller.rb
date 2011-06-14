8ef7ea285eb5bd3e5cbaaf641a471a05fa71e506
Davsebamse <davsebamse@gmail.com>
2011-06-14 22:29:42 +0200

    Updated s.t. it works on iOS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+       if params.has_key? 'post_id' 
+      	redirect_to '/posts/'+params['post_id']
+      	return
+  	end
+      
   end
 
 end
