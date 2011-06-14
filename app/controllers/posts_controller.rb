40b06795b3424a430b37ca77a0e3728e8f62b7c0
Davsebamse <davsebamse@gmail.com>
2011-06-14 16:38:16 +0200

    Updated with danish letters and 404 if wrong post id is entered

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,15 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.xml
   def show
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
+      begin
+          @post = Post.find(params[:id])
+          respond_to do |format|
+              format.html # show.html.erb
+              format.xml  { render :xml => @post }
+          end
+      rescue 
+          render 'public/404', :status => 404, :layout => false
+      end
   end
 
   def answer
