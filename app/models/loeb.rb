cbc8f6af5cdf8259744eeec90032bb471f62f2ab
Davsebamse <davsebamse@gmail.com>
2011-06-29 17:48:35 +0200

    Added middleware to redirect from www. to root

--- app/models/loeb.rb
+++ app/models/loeb.rb
@@ -2,13 +2,6 @@
 class Loeb < ActiveRecord::Base
     has_many :posts
     
-    #Need to figure how to do this in rails 3
-    #HUMANIZED_ATTRIBUTES = {
-    #    :custom_id => "E-mail address"
-    #}
-    #def self.human_attribute_name(attr, number=1)
-    #    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
-    #end
     validates :name, :presence => {:message =>" skal være der"}, :length => { :maximum => 100 }
     validates :contact, :presence => {:message =>" skal være der"}
     validates :custom_id, :presence => {:message =>"løbs id skal være der"}, :uniqueness => {:message =>"løbs id er ikke unikt. Prøv et andet."}, :length => { :maximum => 100 }
