ea235c21cb62ae858b61d89ffbd3a7809084d07d
Frederik Mogensen <sodiaf@gmail.com>
2012-05-01 14:08:16 +0300

    Fixes issue 6..

--- app/models/loeb.rb
+++ app/models/loeb.rb
@@ -8,7 +8,7 @@ class Loeb < ActiveRecord::Base
     
     validates :name, :presence => {:message =>" skal være der"}, :length => { :maximum => 100 }
     validates :contact, :presence => {:message =>" skal være der"}
-    validates :custom_id, :presence => {:message =>"kode skal være der"}, :uniqueness => {:message =>"løbs id er ikke unikt. Prøv et andet."}, :length => { :maximum => 100 }
+    validates :custom_id, :presence => {:message =>"kode skal være der"}, :uniqueness => {:message =>"kode er ikke unik. Prøv en anden."}, :length => { :maximum => 100 }
     
     validates_datetime :start, :before => :slut, :before_message => "må ikke ligge efter slut"
     validates_datetime :slut, :after => :start, :after_message => "må ikke ligge før start"
